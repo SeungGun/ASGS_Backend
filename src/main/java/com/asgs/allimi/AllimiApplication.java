37ac32df47671acd5f81c5bd35e6108bdaa27553
SeungGun <saverrw2864@naver.com>
2024-01-26 17:11:36 +0900

    initial project

---
+++ src/main/java/com/asgs/allimi/AllimiApplication.java
@@ -0,0 +1,13 @@
+package com.asgs.allimi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllimiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllimiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asgs/allimi/AllimiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asgs.allimi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllimiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
