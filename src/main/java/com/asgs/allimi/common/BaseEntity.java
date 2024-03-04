dfc51527237a4ab8912e96c46d114422b393637f
SeungGun <saverrw2864@naver.com>
2024-03-04 22:30:33 +0900

    fix: 초기 정의된 엔티티 필드 수정 (#12)
    * fix: 유저 엔티티 nullable 처리, userRole 필드 추가
    
    * feat: BaseEntity에 create, update 대상 필드 추가

--- src/main/java/com/asgs/allimi/common/BaseEntity.java
+++ src/main/java/com/asgs/allimi/common/BaseEntity.java
@@ -5,7 +5,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -17,9 +19,18 @@ import java.time.LocalDateTime;
 abstract public class BaseEntity {
 
     @CreatedDate
-    @Column(updatable = false)
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @CreatedBy
+    @Column(nullable = false)
+    private String createdBy = "system";
+
     @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false)
+    private String updatedBy = "system";
 }
