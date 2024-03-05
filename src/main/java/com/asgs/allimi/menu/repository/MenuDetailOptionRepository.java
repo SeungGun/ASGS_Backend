f16277773cc3b771b126cc055cf0fb3a326e3c08
SeungGun <saverrw2864@naver.com>
2024-03-05 17:49:59 +0900

    feat: 메뉴 관련 엔티티 기본 repository 인터페이스 정의

---
+++ src/main/java/com/asgs/allimi/menu/repository/MenuDetailOptionRepository.java
@@ -0,0 +1,7 @@
+package com.asgs.allimi.menu.repository;
+
+import com.asgs.allimi.menu.domain.MenuDetailOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuDetailOptionRepository extends JpaRepository<MenuDetailOption, Long> {
+}
---
+++ src/main/java/com/asgs/allimi/menu/repository/MenuOptionRepository.java
@@ -0,0 +1,7 @@
+package com.asgs.allimi.menu.repository;
+
+import com.asgs.allimi.menu.domain.MenuOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuOptionRepository extends JpaRepository<MenuOption, Long> {
+}
