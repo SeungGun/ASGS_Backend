e1eb2b56d750ca3d83c47f57d48735cadb7e0e7e
SeungGun <saverrw2864@naver.com>
2024-03-08 00:19:22 +0900

    feat: 상세 메뉴 조회 API 구현

---
+++ src/main/java/com/asgs/allimi/menu/controller/MenuQueryController.java
@@ -0,0 +1,23 @@
+package com.asgs.allimi.menu.controller;
+
+import com.asgs.allimi.common.response.ResponseForm;
+import com.asgs.allimi.menu.dto.MenuQueryDto;
+import com.asgs.allimi.menu.service.MenuQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/menu")
+public class MenuQueryController {
+
+    private final MenuQueryService menuQueryService;
+
+    @GetMapping("/{menuId}")
+    public ResponseForm<MenuQueryDto.Detail> getDetailMenu(@PathVariable("menuId") Long menuId) {
+        return new ResponseForm<>(menuQueryService.getDetailMenu(menuId));
+    }
+}
